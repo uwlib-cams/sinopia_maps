c19e4f3bd3da1657eb44d101f856d24da6378eef
briesenberg07 <ries07@uw.edu>
2022-02-04 21:04:38 -0800

    rename 'wrapper' script storage2sinopia.py now includes XSLT processing step

---
+++ scripts/storage2sinopia.py
@@ -0,0 +1,17 @@
+
+
+"""propSets to RDF/XML"""
+
+import storage2rdfxml
+
+"""Convert RDF/XML to JSON-LD"""
+
+import rdfxml_to_json
+
+"""Edit JSON-LD for upload to Sinopia"""
+
+import edit_json
+
+"""Post edited JSON-LD to Sinopia"""
+
+import json_to_sinopia
