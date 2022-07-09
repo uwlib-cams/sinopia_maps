54ee50f11ce6cfb0ac12a2e533be7bfccb59022d
briesenberg07 <ries07@uw.edu>
2022-07-08 18:26:31 -0700

    rework 001_delete (from MCM) add option to delete RT IDs from text file successful test today CC @mcm104

--- py/001_delete_rt.py
+++ py/001_delete_rt.py
@@ -1,23 +1,56 @@
+from textwrap import dedent
 import requests
 
-jwt = input("Enter JWT here:\n> ")
-resource_id = input("Enter resource ID here:\n> ")
-sinopia_platform = input("Delete from Development, Stage, or Production?\n[1] Development\n[2] Stage\n[3] Production\n> ")
-if sinopia_platform == "1":
-	sinopia_platform = "development."
-elif sinopia_platform == "2":
-	sinopia_platform = "stage."
-elif sinopia_platform == "3":
-	sinopia_platform = ""
-else:
-	print("Platform not recognized.")
-	quit()
+def intro():
+	print(dedent(f"""
+		DELETING RESOURCES FROM SINOPIA
+		{'=' * 35}
+		"""))
 
-headers = {"Authorization": f"Bearer {jwt}", "Content-Type": "application/json"}
+def prompt_jwt():
+	jwt = input("Enter JWT here:\n> ")
+	prompt_platform(jwt)
+	
+def prompt_platform(jwt):
+	sinopia_platform = input("Delete from Development, Stage, or Production?\n[1] Development\n[2] Stage\n[3] Production\n> ")
+	if sinopia_platform == "1":
+		sinopia_platform = "development."
+	elif sinopia_platform == "2":
+		sinopia_platform = "stage."
+	elif sinopia_platform == "3":
+		sinopia_platform = ""
+	else:
+		print("Platform not recognized.")
+		quit()
+	prompt_resources(jwt, sinopia_platform)
 
-delete_from_sinopia = requests.delete(f"https://api.{sinopia_platform}sinopia.io/resource/{resource_id}", headers = headers)
+def prompt_resources(jwt, platform):
+	resources = input("Delete a single resource or a list?\n('single' or 'list'\n> ")
+	if resources == 'single':
+		resource_id = input("Enter resource ID here:\n> ")
+		delete_single(jwt, platform, resource_id)
+	elif resources == 'list':
+		resource_ids = input("Input filepath to resource ID text file\n> ")
+		delete_list(jwt, platform, resource_ids)
 
-print(delete_from_sinopia.status_code)
+def delete_single(jwt, platform, resource_id):
+	headers = {"Authorization": f"Bearer {jwt}", "Content-Type": "application/json"}
+	delete_from_sinopia = requests.delete(f"https://api.{platform}sinopia.io/resource/{resource_id}", headers = headers)
+	print(delete_from_sinopia.status_code)
+
+def delete_list(jwt, platform, resource_list):
+	to_delete = open(resource_list, 'r')
+	to_delete = to_delete.read().split('\n')
+	for resource in to_delete:
+		if resource != '':
+			headers = {"Authorization": f"Bearer {jwt}", "Content-Type": "application/json"}
+			delete_from_sinopia = requests.delete(f"https://api.{platform}sinopia.io/resource/{resource}", headers = headers)
+			print(delete_from_sinopia.status_code)
+		else:
+			pass
+
+intro()
+prompt_jwt()
 
 # 204 = deleted
 # 401 = Unauthorized
