4d7cffef013ee74ee1d84baee5eef2d6dc10da32
mcm104 <mcm104@uw.edu>
2022-09-27 12:16:23 -0700

    Edit

--- py/003_edit_and_load_RTs.py
+++ py/003_edit_and_load_RTs.py
@@ -1,13 +1,37 @@
 """Step 1: Run XSLT transformation"""
 import os
 
-# Test RTs
-os.system('java -cp ~/saxon11/saxon-he-11.1.jar net.sf.saxon.Transform -t -s:../xsl/001_01_storage_to_rdfxml.xsl -xsl:../xsl/001_01_storage_to_rdfxml.xsl test="TEST:"')
-# Non-test RTs
-os.system('java -cp ~/saxon11/saxon-he-11.1.jar net.sf.saxon.Transform -t -s:../xsl/001_01_storage_to_rdfxml.xsl -xsl:../xsl/001_01_storage_to_rdfxml.xsl')
-# Move to top-level folder
-os.system('mv UWSINOPIA_WAU_rda* ../')
-os.system('mv TEST_UWSINOPIA_WAU_rda* ../')
+def prompt_resources():
+	resources = input("Update test resource templates?\n[1] Update test RTs only\n[2] Update non-test RTs only\n[3] Update all RTs\n> ")
+	acceptable_responses = ["1", "2", "3"]
+	if resources not in acceptable_responses:
+		print("Input not recognized.")
+		quit()
+
+	return resources
+
+resources = prompt_resources()
+
+if resources == 1:
+	# Test RTs
+	os.system('java -cp ~/saxon11/saxon-he-11.1.jar net.sf.saxon.Transform -t -s:../xsl/001_01_storage_to_rdfxml.xsl -xsl:../xsl/001_01_storage_to_rdfxml.xsl test="TEST:"')
+
+	# Move to top-level folder
+	os.system('mv TEST_UWSINOPIA_WAU_rda* ../')
+elif resources == 2:
+	# Non-test RTs
+	os.system('java -cp ~/saxon11/saxon-he-11.1.jar net.sf.saxon.Transform -t -s:../xsl/001_01_storage_to_rdfxml.xsl -xsl:../xsl/001_01_storage_to_rdfxml.xsl')
+
+	# Move to top-level folder
+	os.system('mv UWSINOPIA_WAU_rda* ../')
+else:
+	# Both
+	os.system('java -cp ~/saxon11/saxon-he-11.1.jar net.sf.saxon.Transform -t -s:../xsl/001_01_storage_to_rdfxml.xsl -xsl:../xsl/001_01_storage_to_rdfxml.xsl test="TEST:"')
+	os.system('java -cp ~/saxon11/saxon-he-11.1.jar net.sf.saxon.Transform -t -s:../xsl/001_01_storage_to_rdfxml.xsl -xsl:../xsl/001_01_storage_to_rdfxml.xsl')
+
+	# Move to top-level folder
+	os.system('mv TEST_UWSINOPIA_WAU_rda* ../')
+	os.system('mv UWSINOPIA_WAU_rda* ../')
 
 """Step 2: Convert canonical IRIs to object/datatype IRIs"""
 import functions.replace_canonical_props
