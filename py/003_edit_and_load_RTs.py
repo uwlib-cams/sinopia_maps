967f48f9e1b7b0a53b7c5d6044d68bcf41bb742b
briesenberg07 <ries07@uw.edu>
2022-11-22 14:06:38 -0800

    accomodate different Saxon versions [!] work still needed, as script still assumes Saxon HE directory name and location [!]

--- py/003_edit_and_load_RTs.py
+++ py/003_edit_and_load_RTs.py
@@ -1,5 +1,18 @@
+# NOTES
+	# Run this from sinopia_maps/py 
+	# 2022-11-22: Script now asks for version, but still assumes that Saxon is:
+		# 1) in user's WSL home directory
+		# 2) in a directory named 'saxon11'
+
 """Step 1: Run XSLT transformation"""
 import os
+from textwrap import dedent
+
+saxon_prompt = dedent("""
+Enter the Saxon HE version number you'll use for the transformation
+For example: '11.1', '11.4', etc.
+>""")
+saxon_version = input(saxon_prompt)
 
 def prompt_resources():
 	resources = input("Update test resource templates?\n[1] Update test RTs only\n[2] Update non-test RTs only\n[3] Update all RTs\n> ")
@@ -14,20 +27,20 @@ resources = prompt_resources()
 
 if resources == 1:
 	# Test RTs
-	os.system('java -cp ~/saxon11/saxon-he-11.1.jar net.sf.saxon.Transform -t -s:../xsl/001_01_storage_to_rdfxml.xsl -xsl:../xsl/001_01_storage_to_rdfxml.xsl test="TEST:"')
+	os.system(f'java -cp ~/saxon11/saxon-he-{saxon_version}.jar net.sf.saxon.Transform -t -s:../xsl/001_01_storage_to_rdfxml.xsl -xsl:../xsl/001_01_storage_to_rdfxml.xsl test="TEST:"')
 
 	# Move to top-level folder
 	os.system('mv TEST_UWSINOPIA_WAU_rda* ../')
 elif resources == 2:
 	# Non-test RTs
-	os.system('java -cp ~/saxon11/saxon-he-11.1.jar net.sf.saxon.Transform -t -s:../xsl/001_01_storage_to_rdfxml.xsl -xsl:../xsl/001_01_storage_to_rdfxml.xsl')
+	os.system(f'java -cp ~/saxon11/saxon-he-{saxon_version}.jar net.sf.saxon.Transform -t -s:../xsl/001_01_storage_to_rdfxml.xsl -xsl:../xsl/001_01_storage_to_rdfxml.xsl')
 
 	# Move to top-level folder
 	os.system('mv UWSINOPIA_WAU_rda* ../')
 else:
 	# Both
-	os.system('java -cp ~/saxon11/saxon-he-11.1.jar net.sf.saxon.Transform -t -s:../xsl/001_01_storage_to_rdfxml.xsl -xsl:../xsl/001_01_storage_to_rdfxml.xsl test="TEST:"')
-	os.system('java -cp ~/saxon11/saxon-he-11.1.jar net.sf.saxon.Transform -t -s:../xsl/001_01_storage_to_rdfxml.xsl -xsl:../xsl/001_01_storage_to_rdfxml.xsl')
+	os.system(f'java -cp ~/saxon11/saxon-he-{saxon_version}.jar net.sf.saxon.Transform -t -s:../xsl/001_01_storage_to_rdfxml.xsl -xsl:../xsl/001_01_storage_to_rdfxml.xsl test="TEST:"')
+	os.system(f'java -cp ~/saxon11/saxon-he-{saxon_version}.jar net.sf.saxon.Transform -t -s:../xsl/001_01_storage_to_rdfxml.xsl -xsl:../xsl/001_01_storage_to_rdfxml.xsl')
 
 	# Move to top-level folder
 	os.system('mv TEST_UWSINOPIA_WAU_rda* ../')
