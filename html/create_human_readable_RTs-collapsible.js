0a2ef250e061f1923b19007198fbec7a8f0adc78
paynec4 <48925631+cspayne@users.noreply.github.com>
2023-06-12 17:02:02 -0700

    open toggle on jump link

--- html/create_human_readable_RTs-collapsible.js
+++ html/create_human_readable_RTs-collapsible.js
@@ -7,3 +7,28 @@ for (i = 0; i < toggler.length; i++) {
     this.classList.toggle("caret-down");
   });
 }
+
+
+window.addEventListener("hashchange", function(){
+  var hash = (window.location.hash).substring(1)
+  var el = document.getElementById(hash)
+  var tog = el.getElementsByClassName("caret")
+  for (i = 0; i < tog.length; i++) {
+    if (!tog[i].parentElement.querySelector(".nested").classList.contains("active")) {
+      tog[i].parentElement.querySelector(".nested").classList.toggle("active");
+      tog[i].classList.toggle("caret-down");
+    }
+  }
+})
+
+window.addEventListener("DOMContentLoaded", function(){
+  var hash = (window.location.hash).substring(1)
+  var el = document.getElementById(hash)
+  var tog = el.getElementsByClassName("caret")
+  for (i = 0; i < tog.length; i++) {
+    if (!tog[i].parentElement.querySelector(".nested").classList.contains("active")) {
+      tog[i].parentElement.querySelector(".nested").classList.toggle("active");
+      tog[i].classList.toggle("caret-down");
+    }
+  }
+})
