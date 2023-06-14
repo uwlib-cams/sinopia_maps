286dc40fef1e80b303c0ff409af45503f0f6c946
paynec4 <48925631+cspayne@users.noreply.github.com>
2023-06-14 16:08:47 -0700

    Update create_human_readable_RTs-collapsible.js

--- html/create_human_readable_RTs-collapsible.js
+++ html/create_human_readable_RTs-collapsible.js
@@ -1,6 +1,8 @@
+
+
+/* toggle carets when clicked */
 var toggler = document.getElementsByClassName("caret");
 var i;
-
 for (i = 0; i < toggler.length; i++) {
   toggler[i].addEventListener("click", function() {
     this.parentElement.querySelector(".nested").classList.toggle("active");
@@ -8,6 +10,7 @@ for (i = 0; i < toggler.length; i++) {
   });
 }
 
+/* toggle guidance caret when jump link is clicked (from prop list to prop detail) */
 window.addEventListener("hashchange", function(){
   var hash = (window.location.hash).substring(1)
   var el = document.getElementById(hash)
@@ -21,6 +24,7 @@ window.addEventListener("hashchange", function(){
   jump(hash)
 })
 
+/* toggle guidance caret when external link directs to prop detail */
 window.addEventListener("DOMContentLoaded", function(){
   var hash = (window.location.hash).substring(1)
   var el = document.getElementById(hash)
@@ -34,6 +38,7 @@ window.addEventListener("DOMContentLoaded", function(){
   jump(hash)
 })
 
+/* ensures links jump to top of prop detail */
 function jump(h){
   var url = location.href;
   location.href = "#"+h;              
